51a76203f61b401ef01b2ba78e6adbdf971fb18f
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-17 14:41:00 -0500

    Webtesting-i-challenge - refactoring
    Added refactoring and todo

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,37 +1,41 @@
-const enhancer = require("./enhancer.js");
+const { repair, succeed, fail, get } = require("./enhancer.js");
 // test away!
 
 describe("Enhancer.js", () => {
   // TEST CASE FOR REPAIR FUNCTIONALITY
   describe("Repair", () => {
+    it.todo("Durability value is between 0 and 100");
     it("Durability restored to 100", () => {
-      let item = { name: "Item1", durability: 120, enhancement: 10 };
-      expect(enhancer.repair(item).durability).toBe(100);
+      let item = { name: "Item1", durability: 20, enhancement: 10 };
+      expect(repair(item).durability).toBe(100);
+      expect(repair(item).name).toBe(item.name);
+      expect(repair(item).enhancement).toBe(item.enhancement);
     });
   });
 
   // TEST CASE FOR SUCCEED FUNCTIONALITY
   describe("Success", () => {
     //CHECK IF ENHANCMENT INCREASES BY 1 IF ITS LESS THAN 20
+    it.todo("Enhancement value is between 0 and 20");
     it("The item's enhancement increases to 1 if it is less than 20", () => {
       let item = { name: "Item2", durability: 34, enhancement: 12 };
       let newEnhancement = item.enhancement + 1;
-      console.log(`:: NEW ENHANCEMENT SHOULD BE ${newEnhancement}`);
-      expect(enhancer.succeed(item).enhancement).toBe(newEnhancement);
-      expect(enhancer.succeed(item).durability).toBe(item.durability);
+      console.log(`:: NEW ENHANCEMENT SHOULD BE ${newEnhancement + 1}`);
+      expect(succeed(item).enhancement).toBe(newEnhancement);
+      expect(succeed(item).durability).toBe(item.durability);
     });
 
     //CHECK IF THE ENHANCEMENT REMAINS THE SAME IF ITS EQUAL TO 20
     it("The item's enhancement remains unchanged if it is equal to 20", () => {
       let item = { name: "Item3", durability: 34, enahancement: 20 };
-      expect(enhancer.succeed(item).enhancement).toBe(item.enhancement);
-      expect(enhancer.succeed(item).durability).toBe(item.durability);
+      expect(succeed(item).enhancement).toBe(item.enhancement);
+      expect(succeed(item).durability).toBe(item.durability);
     });
 
     //CHECK IF THE DURABILITY REMAINS THE SAME
     it("Durability of the item remains unchanged", () => {
       let item = { name: "Item4", durability: 34, enahancement: 20 };
-      expect(enhancer.succeed(item).durability).toBe(item.durability);
+      expect(succeed(item).durability).toBe(item.durability);
     });
   });
 
@@ -39,15 +43,15 @@ describe("Enhancer.js", () => {
   describe("Fail", () => {
     it("Item's enhancement is less than 15, durability is decreased by 5", () => {
       let item = { name: "Item5", durability: 45, enhancement: 12 };
-      expect(enhancer.fail(item).durability).toBe(40);
+      expect(fail(item).durability).toBe(40);
     });
     it("Item's enhancement is more than 15, durability is decreased by 10", () => {
       let item = { name: "Item6", durability: 76, enhancement: 17 };
-      expect(enhancer.fail(item).durability).toBe(66);
+      expect(fail(item).durability).toBe(66);
     });
     it("Item's enhancement is more than 16, enhancement is decreased by 1", () => {
       let item = { name: "Item7", durability: 76, enhancement: 17 };
-      expect(enhancer.fail(item).enhancement).toBe(16);
+      expect(fail(item).enhancement).toBe(16);
     });
   });
 
@@ -55,14 +59,14 @@ describe("Enhancer.js", () => {
   describe("Get", () => {
     it("Name is unaltered if enahancement level is 0", () => {
       let item = { name: "Item8", durability: 23, enhancement: 0 };
-      expect(enhancer.get(item).name).toBe(item.name);
+      expect(get(item).name).toBe(item.name);
     });
 
     it("Name is changed to [+durability]Name if enahancement level is greater than 0", () => {
       let item = { name: "Item8", durability: 23, enhancement: 25 };
       let newName = `[+${item.durability}] ${item.name}`;
       //expect(enhancer.get(item).name).toBe(newName);
-      expect(enhancer.get(item).name).toBe(newName);
+      expect(get(item).name).toBe(newName);
     });
   });
 });
