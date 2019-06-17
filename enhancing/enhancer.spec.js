ed6c458187ff77174e08146d2e8344564c523f18
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-17 08:10:24 -0500

    Webtesting-i-challenge - initial set-up
    Installed jest and wrote the high level test cases

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,25 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("Enhancer.js", () => {
+  describe("Repair", () => {
+    it("Durability restored to 100", () => {
+      expect(true).toBe(true);
+    });
+  });
+  describe("Success", () => {
+    it("Modified for enhancement success", () => {
+      expect(false).toBe(false);
+    });
+  });
+  describe("Fail", () => {
+    it("Modified for enhancement failure", () => {
+      expect(false).toBe(false);
+    });
+  });
+  describe("Get", () => {
+    it("Stretch problem - working with get", () => {
+      expect(false).toBe(false);
+    });
+  });
+});
